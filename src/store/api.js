f1c29a74b8ded7bf5b380d6348e222b3e7e7189c
Jivesh210 <78715332+Jivesh210@users.noreply.github.com>
2021-12-25 16:25:34 +0530

    25-12-2021

--- src/store/api.js
+++ src/store/api.js
@@ -7,3 +7,9 @@ export const getMenu = () => {
     `${apiUrl}get-restaurant-items/tandoori-nights-6rvkykyawv4cyzr`
   );
 };
+
+export const register = () => {
+  return axios.post(
+    `${apiUrl}register`
+  );
+};
