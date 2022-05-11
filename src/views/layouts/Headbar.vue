a0d3922a914d9a9ced07fb61b3fedaaa68674023
Jivesh <jivesh@tngproduction.com>
2022-05-11 18:08:07 +0530

    Update Headbar.vue

--- src/views/layouts/Headbar.vue
+++ src/views/layouts/Headbar.vue
@@ -33,7 +33,7 @@
                     <div class="module left">
                         <router-link class="btn btn-outline-secondary" to="/login" v-if="!user">
                             <span class="order">Login</span></router-link>
-                        <router-link class="btn btn-outline-secondary" to="/myaccount" v-if="user && userVerify === true">
+                        <router-link class="btn btn-outline-secondary" to="/myaccount" v-if="user || userVerify === true">
                             <span class="order">My Account</span></router-link>
                     </div>
                 </div>
