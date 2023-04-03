8e1d66ba3e2fb84050b9e51a3ecb664e62915b9c
Ankish Dogra <88072791+Ankish19@users.noreply.github.com>
2023-04-03 16:06:35 +0530

    02-03-2023

--- src/router/index.js
+++ src/router/index.js
@@ -1,199 +1,220 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import { getLocalStorage } from '../store/service'
-import { resendVerifyOtp } from '../store/api'
-import VueMeta from 'vue-meta'
+/*eslint-disable */
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import { getLocalStorage } from "../store/service";
+import { resendVerifyOtp } from "../store/api";
+import VueMeta from "vue-meta";
 
-Vue.use(VueRouter)
-Vue.use(VueMeta)
+Vue.use(VueRouter);
+Vue.use(VueMeta);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
   },
   {
-    path: '/payment-success',
-    name: 'PaymentSuccess',
-    component: () => import('../views/PaymentSuccess.vue')
+    path: "/payment-success",
+    name: "PaymentSuccess",
+    component: () => import("../views/PaymentSuccess.vue"),
   },
   {
-    path: '/menu',
-    name: 'Menu',
-    component: () => import('../views/Menu.vue')
+    path: "/menu",
+    name: "Menu",
+    component: () => import("../views/Menu.vue"),
   },
   {
-    path: '/contact',
-    name: 'Contact',
-    component: () => import('../views/Contact.vue')
+    path: "/contact",
+    name: "Contact",
+    component: () => import("../views/Contact.vue"),
   },
   {
-    path: '/register',
-    name: 'Register',
-    component: () => import('../views/Register.vue')
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/Register.vue"),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
   },
   {
-    path: '/forget',
-    name: 'Forget',
-    component: () => import('../views/Forget.vue')
+    path: "/forget",
+    name: "Forget",
+    component: () => import("../views/Forget.vue"),
   },
   {
-    path: '/otp',
-    name: 'Otp',
-    component: () => import('../views/Otp.vue')
+    path: "/otp",
+    name: "Otp",
+    component: () => import("../views/Otp.vue"),
   },
   {
-    path: '/otpverify',
-    name: 'OtpVerify',
-    component: () => import('../views/OtpVerify.vue')
+    path: "/otpverify",
+    name: "OtpVerify",
+    component: () => import("../views/OtpVerify.vue"),
   },
   {
-    path: '*',
-    name: '404',
-    component: () => import('../views/404.vue')
+    path: "*",
+    name: "404",
+    component: () => import("../views/404.vue"),
   },
+
   // {
   //   path: '/stripe',
   //   name: 'Stripe',
   //   component: () => import('../views/Stripe.vue')
   // },
   {
-    path: '/checkout',
-    name: 'Checkout',
-    component: () => import('../views/CheckOut.vue')
+    path: "/checkout",
+    name: "Checkout",
+    component: () => import("../views/CheckOut.vue"),
   },
   {
-    path: '/checkoutnew',
-    name: 'Checkoutnew',
-    component: () => import('../views/CheckOutNew.vue')
+    path: "/checkoutnew",
+    name: "Checkoutnew",
+    component: () => import("../views/CheckOutNew.vue"),
   },
   {
-    path: '/myaccount',
-    name: 'MyAccount',
-    component: () => import('../views/myaccount/MyAccount.vue')
+    path: "/myaccount",
+    name: "MyAccount",
+    component: () => import("../views/myaccount/MyAccount.vue"),
   },
   {
-    path: '/manageaddress',
-    name: 'ManageAddress',
-    component: () => import('../views/myaccount/ManageAddress.vue')
+    path: "/manageaddress",
+    name: "ManageAddress",
+    component: () => import("../views/myaccount/ManageAddress.vue"),
   },
   {
-    path: '/addmanageaddress',
-    name: 'AddManageAddress',
-    component: () => import('../views/myaccount/AddManageAddress.vue')
+    path: "/addmanageaddress",
+    name: "AddManageAddress",
+    component: () => import("../views/myaccount/AddManageAddress.vue"),
   },
   {
-    path: '/myorder',
-    name: 'MyOrder',
-    component: () => import('../views/myaccount/MyOrder.vue')
+    path: "/myorder",
+    name: "MyOrder",
+    component: () => import("../views/myaccount/MyOrder.vue"),
   },
   {
-    path: '/orderdetails/:id/:uniqueId',
-    name: 'OrderDetails',
-    component: () => import('../views/myaccount/OrderDetails.vue')
+    path: "/orderdetails/:id/:uniqueId",
+    name: "OrderDetails",
+    component: () => import("../views/myaccount/OrderDetails.vue"),
   },
   {
-    path: '/wallet',
-    name: 'Wallet',
-    component: () => import('../views/myaccount/Wallet.vue')
+    path: "/wallet",
+    name: "Wallet",
+    component: () => import("../views/myaccount/Wallet.vue"),
   },
   {
-    path: '/editcart',
-    name: 'EditCart',
-    component: () => import('../views/EditCart.vue')
+    path: "/editcart",
+    name: "EditCart",
+    component: () => import("../views/EditCart.vue"),
   },
   {
-    path: '/change-password',
-    name: 'ChangePassword',
-    component: () => import('../views/ChangePassword.vue')
+    path: "/change-password",
+    name: "ChangePassword",
+    component: () => import("../views/ChangePassword.vue"),
   },
   {
-    path: '/updateprofile',
-    name: 'UpdateProfile',
-    component: () => import('../views/myaccount/UpdateProfile.vue')
+    path: "/updateprofile",
+    name: "UpdateProfile",
+    component: () => import("../views/myaccount/UpdateProfile.vue"),
   },
   {
-    path: '/app-download',
-    name: 'apptemplate',
-    component: () => import('../views/AppTemplate.vue')
+    path: "/app-download",
+    name: "apptemplate",
+    component: () => import("../views/AppTemplate.vue"),
   },
   {
-    path: '/privacypolicy',
-    name: 'privacypolicy',
-    component: () => import('../views/PrivacyPolicy.vue')
+    path: "/privacypolicy",
+    name: "privacypolicy",
+    component: () => import("../views/PrivacyPolicy.vue"),
   },
   {
-    path: '/table-login/:table',
-    name: 'tableLogin',
-    component: () => import('../views/Table_login.vue')
+    path: "/table-login/:table",
+    name: "tableLogin",
+    component: () => import("../views/Table_login.vue"),
   },
   {
-    path: '/success',
-    name: 'success',
-    component: () => import('../views/Success.vue')
+    path: "/success",
+    name: "success",
+    component: () => import("../views/Success.vue"),
   },
   {
-    path: '/failure',
-    name: 'failure',
-    component: () => import('../views/Failure.vue')
+    path: "/failure",
+    name: "failure",
+    component: () => import("../views/Failure.vue"),
   },
   {
-    path: '/cancel',
-    name: 'cancel',
-    component: () => import('../views/Cancel.vue')
-  }
-]
+    path: "/cancel",
+    name: "cancel",
+    component: () => import("../views/Cancel.vue"),
+  },
+  {
+    path: "/search",
+    name: "search",
+    component: () => import("@/views/Search.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
 router.beforeEach((to, _, next) => {
-  const userData = getLocalStorage('userData')
-  const userDataVerify = getLocalStorage('userDataVerify') ?? false
+  const userData = getLocalStorage("userData");
+  const userDataVerify = getLocalStorage("userDataVerify") ?? false;
 
   if (userData) {
-    if (to.path === '/login' || to.path === '/register' || to.path === '' || to.path === '/forget' || to.path === '/change-password') {
-      return next('/')
-    } else if (userData.verified_at === null || userDataVerify === 'false') {
-      if (to.path !== '/otpverify') {
+    if (
+      to.path === "/login" ||
+      to.path === "/register" ||
+      to.path === "" ||
+      to.path === "/forget" ||
+      to.path === "/change-password"
+    ) {
+      return next("/");
+    } else if (userData.verified_at === null || userDataVerify === "false") {
+      if (to.path !== "/otpverify") {
         // console.log(userData.verified_at)
         const form = {
           email: userData.email,
-          phone: userData.phone
-        }
-        resendVerifyOtp(form).then(res => {
-          console.log(res.data)
-        })
+          phone: userData.phone,
+        };
+        resendVerifyOtp(form).then((res) => {
+          console.log(res.data);
+        });
         return next({
-          path: '/otpverify',
-          query: { redirect: 'email' }
-        })
+          path: "/otpverify",
+          query: { redirect: "email" },
+        });
       } else {
-        return next()
+        return next();
       }
     }
   } else if (!userData) {
-    if (to.path === '/myaccount' || to.path === '/checkout' || to.path === '/editcart' || to.path === '/ManageAddress' || to.path === '/addmanageaddress' || to.path === '/myorder' || to.path === '/wallet') {
-      return next('/login')
+    if (
+      to.path === "/myaccount" ||
+      to.path === "/checkout" ||
+      to.path === "/editcart" ||
+      to.path === "/ManageAddress" ||
+      to.path === "/addmanageaddress" ||
+      to.path === "/myorder" ||
+      to.path === "/wallet"
+    ) {
+      return next("/login");
     }
   } else {
-    return next()
+    return next();
   }
-  return next()
-})
-export default router
+  return next();
+});
+export default router;
---
+++ src/views/Search.vue
@@ -0,0 +1,35 @@
+/*eslint-disable */
+<!--** View Section **-->
+<template>
+  <div id="search"></div>
+</template>
+
+<!--** Logic Section **-->
+<script>
+/*eslint-disable */
+//  Import Section  //
+
+export default {
+  //  My Route  //
+  name: "search",
+
+  //  Components  //
+  components: {},
+
+  //  Init  //
+  mounted() {
+    this.$router.push({ name: "Home" });
+  },
+
+  //  Variables  //
+  data() {
+    return {};
+  },
+
+  //  Functions  //
+  methods: {},
+};
+</script>
+
+<!--** Style Section **-->
+<style lang="scss" scoped></style>
